84e1d78a726e7cb71b5a381b65d4fce62ba6e489
Luiz Gustavo <luiz.gustavo@pasi.com.br>
2025-01-08 11:31:48 -0300

    feat: adding types to the function return

--- Exercises/FunctionTypes/FunctionTypes.ts
+++ Exercises/FunctionTypes/FunctionTypes.ts
@@ -23,11 +23,11 @@
 // hint - use modulo
 
 
-const twoFer = (name: string = "you") => {
+const twoFer = (name: string = "you"): string => {
     return `One for ${name}, one for me`;
 }
 
-const isLeapYear = (year: number) => {
+const isLeapYear = (year: number): boolean => {
     return year % 4 === 0 && year % 100 !== 0 || year % 400 === 0;
 }
 
